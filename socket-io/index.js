f6fcca8ac6e47c699842d9ea3a9754e1d365ab8e
Akash Vishwakarma <107378176+akash2v@users.noreply.github.com>
2025-02-08 23:59:37 +0530

    updating Ui and Server

--- socket-io/index.js
+++ socket-io/index.js
@@ -1,27 +1,35 @@
 const http = require("http");
-
-const express = require("express")
-// const publicIp = require('public-ip');
-const path = require("path");
+const express = require("express");
 const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (client) => {
-    console.log("New user connected " + client.id);
+    console.log("New user connected:", client.id);
+
+    // Send the correct live user count
+    io.emit("online", io.engine.clientsCount);
+
+    client.on("disconnect", () => {
+        console.log("User disconnected:", client.id);
+        io.emit("online", io.engine.clientsCount);
+    });
+
+    // Handle messages
     client.on("message", (message) => {
-        console.log(message);
+        console.log("Received:", message);
         io.emit("response", `<h3>${message}</h3>`);
     });
 });
 
-app.use(express.static(path.resolve("./public")));
+// Serve static files
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-    res.sendFile("./public/");
-})
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 server.listen(9000, () => {
     console.log("Server Started at Port 9000");
